72d4f3eb01e83e70784814a8bb311bcb9fca40f1
sethuramv <sethuram.v@appoets.com>
2021-03-02 12:12:18 +0530

    Updated Code

---
+++ TimerAppExample/Time.swift
@@ -0,0 +1,14 @@
+//
+//  Time.swift
+//  TimerAppExample
+//
+//  Created by Apple on 13/02/21.
+//
+
+import Foundation
+import CoreData
+
+ class Time: NSManagedObject {
+    
+     @NSManaged var timeStr: String?
+}
